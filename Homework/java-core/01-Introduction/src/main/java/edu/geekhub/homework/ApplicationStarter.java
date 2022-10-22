b508b9466000399c820d7865149b9a6482cc23b7
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-10-22 12:14:31 +0300

    L01: Add output "Hello World!" message into console. In class ApplicationStarter, im method main(), was added output "Hello World!" message into console.

--- Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,14 @@
 package edu.geekhub.homework;
 
+/**
+ * L01.HW01
+ * Class with a starting point for print "Hello World!".
+ *
+ * @author Bohdan Lifirenko
+ */
 public class ApplicationStarter {
 
-    // Write code here :)
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
