ffd1a454a653e628238e98914efee2ef2c8a8732
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-06 23:16:21 +0200

    L03 Update: Add method getPositiveDoubleFromUser
    Created getPositiveDoubleFromUser method to get characteristics for
    shape classes from user. And because characteristics of the shape
    classes must be only positive real number, I created a
    checkIsInputPositiveRealNumber. And because the characteristics will
    be stored in variables of type double I created checkIsInputDouble.

--- Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/user/input/TakerInputFromUser.java
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/user/input/TakerInputFromUser.java
@@ -1,10 +1,36 @@
 package edu.geekhub.homework.user.input;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class TakerInputFromUser {
 
     private TakerInputFromUser() {}
+
+    public static double getPositiveDoubleFromUser(String massageToUser) {
+        String input = getInputFormUser(massageToUser);
+
+        boolean notPositiveDouble = true;
+        while (notPositiveDouble) {
+            if (!checkIsInputPositiveRealNumber(input)) {
+                System.out.println("You entered non-positive real number. ");
+
+                input = getInputFormUser(massageToUser);
+                continue;
+            }
+
+            if (!checkIsInputDouble(input)) {
+                System.out.println("You entered a number outside the range of double values.");
+
+                input = getInputFormUser(massageToUser);
+                continue;
+            }
+
+            notPositiveDouble = false;
+        }
+
+        return Double.parseDouble(input);
+    }
 
     public static String getInputFormUser(String massageToUser) {
         Scanner scanner = new Scanner(System.in);
@@ -12,5 +38,18 @@ public class TakerInputFromUser {
         System.out.print(massageToUser);
 
         return scanner.nextLine();
+    }
+
+    private static boolean checkIsInputPositiveRealNumber(String userInput) {
+        Scanner scanner = new Scanner(userInput);
+        Pattern integerLiteralInTheEndOfLine = Pattern.compile("\\d+(\\.\\d+)?$");
+
+        return scanner.hasNext(integerLiteralInTheEndOfLine);
+    }
+
+    private static boolean checkIsInputDouble(String userInput) {
+        Scanner scanner = new Scanner(userInput);
+
+        return scanner.hasNextDouble();
     }
 }
