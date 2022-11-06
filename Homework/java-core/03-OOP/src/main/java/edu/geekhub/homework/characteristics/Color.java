1ae0cbc5344571f6ea9dc008d3c4be087440d9a6
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-06 20:07:29 +0200

    L03 Update: Create class Color

---
+++ Homework/java-core/03-OOP/src/main/java/edu/geekhub/homework/characteristics/Color.java
@@ -0,0 +1,15 @@
+package edu.geekhub.homework.characteristics;
+
+public enum Color {
+    BLACK, RED, BLUE, GREEN, YELLOW, MAGENTA;
+
+    private static final int ORDINAL_OF_FIRST_ENUMERATION_CONSTANT = 0;
+
+    public static int getOrdinalOfFirstEnumerationConstant() {
+        return ORDINAL_OF_FIRST_ENUMERATION_CONSTANT;
+    }
+
+    public static int getOrdinalOfLastEnumerationConstant() {
+        return Color.values().length - 1;
+    }
+}
