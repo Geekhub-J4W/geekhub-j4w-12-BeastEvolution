5b02b03a6e4160a92f6f8c10b0cf4f15fc2642f3
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 02:39:07 +0200

    L04 Up:Create AmountOfFollowersValidationException

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/validation/user/exception/AmountOfFollowersValidationException.java
@@ -0,0 +1,17 @@
+package edu.geekhub.homework.validation.user.exception;
+
+import edu.geekhub.homework.validation.exception.ValidationException;
+
+public class AmountOfFollowersValidationException extends ValidationException {
+    public AmountOfFollowersValidationException(String message) {
+        super(message);
+    }
+
+    public AmountOfFollowersValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AmountOfFollowersValidationException(Throwable cause) {
+        super(cause);
+    }
+}
