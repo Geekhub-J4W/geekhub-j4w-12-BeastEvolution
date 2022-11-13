8df57b3940f455052e0bc1ebe54a2c9adff419b2
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 02:29:32 +0200

    L04 Up: Create class ValidationExceptionArrayList
    To output to the console a list of errors when validating User fields

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/collections/ValidationExceptionArrayList.java
@@ -0,0 +1,23 @@
+package edu.geekhub.homework.collections;
+
+import edu.geekhub.homework.validation.exception.ValidationException;
+import java.util.Arrays;
+
+public class ValidationExceptionArrayList {
+    private ValidationException[] exceptions = new ValidationException[0];
+
+    public void addToEnd(ValidationException e) {
+        ValidationException[] newExceptionsArray = Arrays.copyOf(exceptions, exceptions.length + 1);
+        newExceptionsArray[newExceptionsArray.length - 1] = e;
+
+        exceptions = newExceptionsArray;
+    }
+
+    public Exception get(int index) {
+        return exceptions[index];
+    }
+
+    public int sizeOfList() {
+        return exceptions.length;
+    }
+}
