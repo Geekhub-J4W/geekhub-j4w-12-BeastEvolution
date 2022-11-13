7983524b55795797834e954d928c5ff059813576
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 03:14:20 +0200

    L04 Ch:Realize class UserController

--- Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserController.java
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserController.java
@@ -1,6 +1,10 @@
 package edu.geekhub.homework;
 
 import edu.geekhub.controller.Controller;
+import edu.geekhub.homework.exceptions.RepositorySavingException;
+import edu.geekhub.homework.validation.RequestValidator;
+import edu.geekhub.homework.validation.exception.ValidationException;
+import edu.geekhub.models.User;
 import edu.geekhub.models.request.Request;
 import edu.geekhub.models.request.Response;
 
@@ -8,10 +12,32 @@ import edu.geekhub.models.request.Response;
 public class UserController implements Controller {
 
     private final UserService service = new UserService();
+    private final RequestValidator requestValidator = new RequestValidator();
 
     @Override
     public Response process(Request request) {
-        //ToDo Write homework-related code here
-        return null;
+        try {
+            requestValidator.validateResponseData(request);
+
+            boolean isUserNotSaveInRepository =
+                !(tryToSaveUserInRepository((User) request.getData()));
+            if (isUserNotSaveInRepository) {
+                return Response.fail(request.getData());
+            }
+
+            return Response.ok(request.getData());
+        } catch (ValidationException e) {
+            System.out.println(e.getMessage());
+            return Response.fail();
+        }
+    }
+
+    private boolean tryToSaveUserInRepository(User user) {
+        try {
+            service.saveUserToRepository(user);
+            return true;
+        } catch (RepositorySavingException e) {
+            return false;
+        }
     }
 }
