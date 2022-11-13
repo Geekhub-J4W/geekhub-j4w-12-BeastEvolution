b3da65b020e7b9fed4af6ccf5962f115fb707e69
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 03:03:42 +0200

    L04 Up:Create class RequestValidationException

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/validation/exception/RequestValidationException.java
@@ -0,0 +1,15 @@
+package edu.geekhub.homework.validation.exception;
+
+public class RequestValidationException extends ValidationException {
+    public RequestValidationException(String message) {
+        super(message);
+    }
+
+    public RequestValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RequestValidationException(Throwable cause) {
+        super(cause);
+    }
+}
