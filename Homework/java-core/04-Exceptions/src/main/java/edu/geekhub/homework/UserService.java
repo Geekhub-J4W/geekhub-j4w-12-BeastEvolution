bcb1e59e9cd71a1b7a672a9e718840d6553d1fb3
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 03:08:38 +0200

    L04 Ch:Realize class UserService

--- Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserService.java
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserService.java
@@ -1,12 +1,28 @@
 package edu.geekhub.homework;
 
-import edu.geekhub.storage.Repository;
+import edu.geekhub.exceptions.ConnectionInterruptedException;
+import edu.geekhub.homework.exceptions.RepositorySavingException;
+import edu.geekhub.homework.validation.user.UserValidator;
+import edu.geekhub.homework.validation.user.exception.UserValidationException;
+import edu.geekhub.models.User;
 import edu.geekhub.storage.MemoryStorage;
+import edu.geekhub.storage.Repository;
 
 // Don't move this class
 public class UserService {
 
     private final Repository repository = new MemoryStorage();
+    private final UserValidator userValidator = new UserValidator(repository);
 
-    //ToDo Write homework-related code here
+    public void saveUserToRepository(User user) {
+        try {
+            userValidator.validateUser(user);
+            repository.tryToAdd(user);
+        } catch (ConnectionInterruptedException e) {
+            throw new RepositorySavingException(e.getMessage(), e);
+        } catch (UserValidationException e) {
+            e.printExceptionInfo();
+            throw new RepositorySavingException(e.getMessage(), e);
+        }
+    }
 }
