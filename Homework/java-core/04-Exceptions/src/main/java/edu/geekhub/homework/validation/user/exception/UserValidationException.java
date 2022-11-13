f40958b436b54e43c77e8611fc3e0986b7402123
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-13 02:33:32 +0200

    L04 Up: Create class UserValidationException

---
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/validation/user/exception/UserValidationException.java
@@ -0,0 +1,43 @@
+package edu.geekhub.homework.validation.user.exception;
+
+import edu.geekhub.homework.collections.ValidationExceptionArrayList;
+import edu.geekhub.homework.validation.exception.ValidationException;
+
+public class UserValidationException extends ValidationException {
+
+    private ValidationExceptionArrayList validationExceptionArrayList =
+        new ValidationExceptionArrayList();
+
+    public UserValidationException(String message) {
+        super(message);
+    }
+
+    public UserValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public void addValidationException(ValidationException validationException) {
+        validationExceptionArrayList.addToEnd(validationException);
+    }
+
+    public void printExceptionInfo() {
+        System.out.println(this);
+        System.out.println("Consist of:");
+        printValidationExceptionList();
+    }
+
+    private void printValidationExceptionList() {
+        for (int i = 0; i < validationExceptionArrayList.sizeOfList(); i++) {
+            System.out.println(i + ": " + validationExceptionArrayList.get(i).getMessage());
+        }
+    }
+
+    public boolean checkWhetherValidationErrorsPresent() {
+
+        return (validationExceptionArrayList.sizeOfList() > 0);
+    }
+}
