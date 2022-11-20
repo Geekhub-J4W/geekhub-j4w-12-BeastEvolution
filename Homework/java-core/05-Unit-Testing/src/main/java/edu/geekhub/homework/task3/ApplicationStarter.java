a2ba57f737a1232b8cad9e3677e6cb0cf70eb5ec
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-20 02:03:43 +0200

    HM05.T3 Up: Realize class ApplicationStarter

--- Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task3/ApplicationStarter.java
+++ Homework/java-core/05-Unit-Testing/src/main/java/edu/geekhub/homework/task3/ApplicationStarter.java
@@ -2,5 +2,16 @@ package edu.geekhub.homework.task3;
 
 public class ApplicationStarter {
 
-    // Write code here :)
+    public static void main(String[] args) {
+        SequenceCalculator sequenceCalculator = new SequenceCalculator();
+        try {
+            System.out.println(sequenceCalculator.calculate(
+                "1adasd,adad2,-3",
+                ArithmeticOperation.MULTIPLICATION)
+            );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
