d4077d4f8540008130b3026bd327197099cff332
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-23 22:42:02 +0200

    HM06 Fix: rename OrcoStatTest
    Name of test class must match tested class

--- Homework/java-core/06-Practice/src/test/java/edu/geekhub/orcostat/OrcoStatServiceTest.java
+++ Homework/java-core/06-Practice/src/test/java/edu/geekhub/orcostat/OrcoStatServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class OrcoStatTest {
+public class OrcoStatServiceTest {
 
     private OrcoStatService orcoStatService;
 
