3b1564839975a48d8b1f41a0980cca50c144cb42
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-27 02:04:32 +0200

    HM06 Up:create ActionMenuNode

---
+++ Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/menu/node/ActionMenuNode.java
@@ -0,0 +1,7 @@
+package edu.geekhub.orcostat.menu.node;
+
+public class ActionMenuNode extends MenuNode {
+    public ActionMenuNode(String id, int number, String name) {
+        super(id, number, name);
+    }
+}
