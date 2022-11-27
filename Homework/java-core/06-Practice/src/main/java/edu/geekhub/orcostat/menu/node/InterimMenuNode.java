a0e2c2a4decfe39be34e6ebbc3b8a63c9e35407d
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-27 02:03:49 +0200

    HM06 Up:create InterimMenuNode

---
+++ Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/menu/node/InterimMenuNode.java
@@ -0,0 +1,7 @@
+package edu.geekhub.orcostat.menu.node;
+
+public class InterimMenuNode extends MenuNode {
+    public InterimMenuNode(String id, int number, String name) {
+        super(id, number, name);
+    }
+}
