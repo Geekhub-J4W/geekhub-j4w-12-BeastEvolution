5ca902b5905b985c992d5790e67777f98962d674
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-11-27 02:07:59 +0200

    HM06 Up:create BeckMenuNode

---
+++ Homework/java-core/06-Practice/src/main/java/edu/geekhub/orcostat/menu/node/BeckMenuNode.java
@@ -0,0 +1,7 @@
+package edu.geekhub.orcostat.menu.node;
+
+public class BeckMenuNode extends MenuNode {
+    public BeckMenuNode(String id, int number, String name) {
+        super(id, number, name);
+    }
+}
