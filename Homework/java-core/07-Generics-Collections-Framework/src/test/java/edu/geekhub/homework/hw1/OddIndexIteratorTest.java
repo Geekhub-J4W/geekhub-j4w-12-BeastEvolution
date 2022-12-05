5f1faa4c1ba6d90ec81f8dbdd4b3cafed9ba7691
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-05 10:28:20 +0200

    HM07 UP:add test for method next

--- Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw1/OddIndexIteratorTest.java
+++ Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw1/OddIndexIteratorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 class OddIndexIteratorTest {
     OddIndexIterator<Object> objectOddIndexIterator;
@@ -47,5 +48,40 @@ class OddIndexIteratorTest {
         boolean isHasNextElement = objectOddIndexIterator.hasNext();
 
         assertTrue(isHasNextElement);
+    }
+
+    @Tag("normal work")
+    @Tag("next()")
+    @Test
+    void return_next_when_have_odd_elements() {
+        List<Object> objects = new ArrayList<>();
+        Object oddElement = new Object();
+        objects.add(new Object());
+        objects.add(oddElement);
+        objectOddIndexIterator = new OddIndexIterator<>(objects);
+
+        Object nextOddElement = objectOddIndexIterator.next();
+
+        assertEquals(oddElement, nextOddElement);
+    }
+
+    @Tag("normal work")
+    @Tag("next()")
+    @Test
+    void exception_when_left_even_element() {
+        List<Object> objects = new ArrayList<>();
+        objects.add(new Object());
+        objectOddIndexIterator = new OddIndexIterator<>(objects);
+
+        assertThrows(NoSuchElementException.class, () -> objectOddIndexIterator.next());
+    }
+
+    @Tag("normal work")
+    @Tag("hasNext()")
+    @Test
+    void exception_when_no_more_elements() {
+        objectOddIndexIterator = new OddIndexIterator<>(new ArrayList<>());
+
+        assertThrows(NoSuchElementException.class, () -> objectOddIndexIterator.next());
     }
 }
