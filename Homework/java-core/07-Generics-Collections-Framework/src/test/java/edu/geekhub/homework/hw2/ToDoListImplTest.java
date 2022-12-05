af13908f4a11cab46fde3ca31300288d270f987b
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-05 12:18:09 +0200

    HM07 FIX:fix style problem

--- Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw2/ToDoListImplTest.java
+++ Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw2/ToDoListImplTest.java
@@ -4,7 +4,6 @@ import edu.geekhub.homework.hw2.entity.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,20 +24,20 @@ class ToDoListImplTest {
     void add_task_to_end_of_list() {
         Task lastTaskInList = new Task("Task2", "Text", 2);
         taskToDoList.addTaskToTheEnd(new Task("Task", "Text", 1));
-        boolean resultOFAdding = taskToDoList.addTaskToTheEnd(lastTaskInList);
+        boolean resultOfAdding = taskToDoList.addTaskToTheEnd(lastTaskInList);
 
         assertEquals(lastTaskInList, taskToDoList.getAllTasks().get(1));
-        assertTrue(resultOFAdding);
+        assertTrue(resultOfAdding);
     }
 
     @Test
     @Tag("null")
     @Tag("addTaskToTheEnd")
     void add_null_to_end_of_list() {
-        boolean resultOFAdding = taskToDoList.addTaskToTheEnd(null);
+        boolean resultOfAdding = taskToDoList.addTaskToTheEnd(null);
 
         assertNull(taskToDoList.getAllTasks().get(0));
-        assertTrue(resultOFAdding);
+        assertTrue(resultOfAdding);
     }
 
     @Test
