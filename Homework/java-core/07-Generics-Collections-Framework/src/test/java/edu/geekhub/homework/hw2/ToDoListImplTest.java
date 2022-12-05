9c9ec24c1f5698454a72110c992f9deaa7a58a1b
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-05 10:31:29 +0200

    HM07 UP:add setUp

--- Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw2/ToDoListImplTest.java
+++ Homework/java-core/07-Generics-Collections-Framework/src/test/java/edu/geekhub/homework/hw2/ToDoListImplTest.java
@@ -1,17 +1,25 @@
 package edu.geekhub.homework.hw2;
 
 import edu.geekhub.homework.hw2.entity.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToDoListImplTest {
+
+    ToDoListImpl<Task> taskToDoList;
+
+    @BeforeEach
+    void setUp() {
+        taskToDoList = new ToDoListImpl<>();
+    }
+
     @Test
     @Tag("correct work")
     @Tag("addTaskToTheEnd")
     void add_task_to_end_of_list() {
-        ToDoListImpl<Task> taskToDoList = new ToDoListImpl<>();
         Task lastTaskInList = new Task("Task2", "Text", 2);
         taskToDoList.addTaskToTheEnd(new Task("Task", "Text", 1));
         boolean resultOFAdding = taskToDoList.addTaskToTheEnd(lastTaskInList);
@@ -24,7 +32,6 @@ class ToDoListImplTest {
     @Tag("correct work")
     @Tag("addTaskToTheEnd")
     void add_null_to_end_of_list() {
-        ToDoListImpl<Task> taskToDoList = new ToDoListImpl<>();
         boolean resultOFAdding = taskToDoList.addTaskToTheEnd(null);
 
         assertEquals(null, taskToDoList.getAllTasks().get(0));
