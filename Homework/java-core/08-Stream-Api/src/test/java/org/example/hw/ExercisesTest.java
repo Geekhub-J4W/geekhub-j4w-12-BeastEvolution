415d2308f07faf133914ba9983bc6a3e627e3b06
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-12 20:16:27 +0200

    HM8 UP:add tests for mostPopulatedCity()

---
+++ Homework/java-core/08-Stream-Api/src/test/java/org/example/hw/ExercisesTest.java
@@ -0,0 +1,40 @@
+package org.example.hw;
+
+import org.example.hw.exceptions.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExercisesTest {
+
+    private Exercises exercises;
+
+    @BeforeEach
+    void setUp() {
+        exercises = new Exercises(new CitiesImp());
+    }
+
+    @Test
+    @Tag("correct work")
+    @Tag(("mostPopulatedCity"))
+    void get_most_populated_city() {
+        City expectedCity = new City(1024, "Mumbai (Bombay)", "IND", 10500000);
+
+        City actualCity = exercises.mostPopulatedCity();
+
+        assertEquals(expectedCity, actualCity);
+    }
+
+    @Test
+    @Tag("error")
+    @Tag(("mostPopulatedCity"))
+    void fail_find_most_populated_city() {
+        exercises = new Exercises(new CitiesMock());
+
+        NotFoundException thrown = assertThrows(NotFoundException.class, exercises::mostPopulatedCity);
+
+        assertEquals("No cities found", thrown.getMessage());
+    }
+}
