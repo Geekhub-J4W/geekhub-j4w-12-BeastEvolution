eebfc3ff4094529552d9a84dc256f1a98710f751
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-20 01:00:42 +0200

    HM9 UP:implement findStatisticWithMaxLosesAmounts

--- Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/analytics/AnalyticsService.java
+++ Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/analytics/AnalyticsService.java
@@ -1,18 +1,37 @@
 package edu.geekhub.homework.analytics;
 
-import edu.geekhub.homework.domain.LosesStatistic;
+import static edu.geekhub.homework.util.NotImplementedException.TODO_TYPE;
 
+import edu.geekhub.homework.domain.LosesStatistic;
+import edu.geekhub.homework.domain.StatisticItem;
+import java.util.Comparator;
 import java.util.List;
 
-import static edu.geekhub.homework.util.NotImplementedException.TODO;
-import static edu.geekhub.homework.util.NotImplementedException.TODO_TYPE;
 
 /**
  * Service shows interesting analytics information
  */
 public class AnalyticsService {
     public LosesStatistic findStatisticWithMaxLosesAmounts(List<LosesStatistic> losesStatistics) {
-        return TODO_TYPE("Implement method");
+        return losesStatistics.stream()
+            .max(new LosesStatisticAmountsComparator())
+            .orElseThrow(() -> new IllegalArgumentException(
+                "List of Statistic is empty"
+            ));
+    }
+
+    class LosesStatisticAmountsComparator implements Comparator<LosesStatistic> {
+        @Override
+        public int compare(LosesStatistic o1, LosesStatistic o2) {
+            int o1LosesAmounts = o1.getStatisticItems().stream()
+                .mapToInt(StatisticItem::value)
+                .sum();
+            int o2LosesAmounts = o2.getStatisticItems().stream()
+                .mapToInt(StatisticItem::value)
+                .sum();
+
+            return o1LosesAmounts - o2LosesAmounts;
+        }
     }
 
     public LosesStatistic findStatisticWithMinLosesAmounts(List<LosesStatistic> losesStatistics) {
