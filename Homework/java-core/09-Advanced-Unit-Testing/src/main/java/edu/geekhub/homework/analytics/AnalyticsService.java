cebefecde7783c6804b8bcf107d95b2e6c94eab7
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-20 03:26:44 +0200

    HM9 UP:implement totalCountOfLosesForStatistic

--- Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/analytics/AnalyticsService.java
+++ Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/analytics/AnalyticsService.java
@@ -43,7 +43,11 @@ public class AnalyticsService {
     }
 
     public int totalCountOfLosesForStatistic(LosesStatistic losesStatistic) {
-        return TODO_TYPE("Implement method");
+        return losesStatistic
+            .getStatisticItems()
+            .stream()
+            .mapToInt(StatisticItem::value)
+            .sum();
     }
 
     public int totalCountOfLosesForAllStatistics(List<LosesStatistic> losesStatistics) {
