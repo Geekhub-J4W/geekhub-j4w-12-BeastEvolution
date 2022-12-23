4c03365d9418b229e1904b168d18a0a193551514
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-12-23 17:36:09 +0200

    HM9 UP:implement main()

--- Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/09-Advanced-Unit-Testing/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,27 +1,37 @@
 package edu.geekhub.homework;
 
 import edu.geekhub.homework.analytics.AnalyticsService;
+import edu.geekhub.homework.client.JsonConverter;
+import edu.geekhub.homework.client.LosesStatisticHttpClient;
 import edu.geekhub.homework.domain.LosesStatisticService;
+import java.util.Random;
 
-import static edu.geekhub.homework.util.NotImplementedException.TODO_TYPE;
 
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        // Write code here :)
+        var losesStatisticService = new LosesStatisticService(
+            new LosesStatisticHttpClient(),
+            new JsonConverter()
+        );
 
-        var losesStatisticService = new LosesStatisticService();
+        var losesStatistics = losesStatisticService.getAll();
+        var randomStatistic = new Random().nextInt(losesStatistics.size());
 
-        var losesStatistic = losesStatisticService.getById(TODO_TYPE());
+        var losesStatistic = losesStatisticService.getById(
+            losesStatistics
+                .get(randomStatistic)
+                .id()
+        );
         losesStatisticService.create(losesStatistic);
         losesStatisticService.deleteById(losesStatistic.id());
 
         var analyticsService = new AnalyticsService();
 
-        print(analyticsService.totalCountOfLosesForStatistic(TODO_TYPE()));
-        print(analyticsService.totalCountOfLosesForAllStatistics(TODO_TYPE()));
-        print(analyticsService.findStatisticWithMinLosesAmounts(TODO_TYPE()));
-        print(analyticsService.findStatisticWithMaxLosesAmounts(TODO_TYPE()));
+        print(analyticsService.totalCountOfLosesForStatistic(losesStatistic));
+        print(analyticsService.totalCountOfLosesForAllStatistics(losesStatisticService.getAll()));
+        print(analyticsService.findStatisticWithMinLosesAmounts(losesStatisticService.getAll()));
+        print(analyticsService.findStatisticWithMaxLosesAmounts(losesStatisticService.getAll()));
 
         losesStatisticService.deleteAll();
     }
