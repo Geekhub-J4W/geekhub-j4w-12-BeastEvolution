2920663eabda1a3de49282d2e2f32ddd84d10608
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-06 16:10:20 +0200

    HM13_UP:improve PropertyParser realisation

--- Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/parsers/PropertyParser.java
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/parsers/PropertyParser.java
@@ -4,14 +4,39 @@ import edu.geekhub.homework.entity.Property;
 
 public class PropertyParser {
     public Property getProperty(String line) {
-        String[] propertyData = line.split("=");
-        propertyData[0] = deletePropertyNamePrefix(propertyData[0]);
+        if (line == null || line.isEmpty()) {
+            throw new IllegalArgumentException("Input line cannot be null or empty");
+        }
 
-        return new Property(propertyData[0], propertyData[1]);
+        String[] propertyData = line.split("=", -1);
+        if (propertyData.length != 2) {
+            throw new IllegalArgumentException("Incorrect input line format. It must contain exactly one '=' symbol");
+        }
+
+        String name = validateName(propertyData[0]);
+        String value = validateValue(propertyData[1]);
+
+        return new Property(name, value);
     }
 
-    private String deletePropertyNamePrefix(String namePath) {
-        String[] elementsOfPropertyNamePath = namePath.split("\\.");
+    private String validateName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        String[] elementsOfPropertyNamePath = name.split("\\.", -1);
+        for (String element : elementsOfPropertyNamePath) {
+            if (element.isEmpty()) {
+                throw new IllegalArgumentException("Namespace cannot contain empty elements separated by '.'");
+            }
+        }
         return elementsOfPropertyNamePath[elementsOfPropertyNamePath.length - 1];
+    }
+
+    private String validateValue(String value) {
+        if (value.isEmpty()) {
+            throw new IllegalArgumentException("Value cannot be empty");
+        }
+        return value;
     }
 }
