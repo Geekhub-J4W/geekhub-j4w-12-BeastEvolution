e312ddac091f97d69094749732d042a5c6951c82
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-06 16:12:49 +0200

    HM13_UP:add logging to PropertyService

--- Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/entity/PropertyService.java
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/entity/PropertyService.java
@@ -6,19 +6,37 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public class PropertyService {
+    private final Logger logger;
+
+    public PropertyService(Logger logger) {
+        this.logger = logger;
+    }
+
     public List<Property> getPropertiesFromFile(byte[] fileData) {
         if (Objects.isNull(fileData)) {
             throw new IllegalArgumentException("File data can't be null");
         }
         String fileText = new String(fileData, StandardCharsets.UTF_8);
-        String[] fileLines = fileText.split(System.getProperty("line.separator"));
+        String[] fileLines = fileText.split("\n");
 
         PropertyParser propertyParser = new PropertyParser();
         return Arrays.stream(fileLines)
-            .map(propertyParser::getProperty)
+            .map(line -> {
+                try {
+                    return propertyParser.getProperty(line);
+                } catch (IllegalArgumentException e) {
+                    logger.warning(
+                        String.format("%s:%s%n", line, e.getMessage())
+                    );
+                    return null;
+                }
+            })
+            .filter(Objects::nonNull)
             .toList();
+
     }
 
 }
--- Homework/java-core/13-Reflection-Api/src/test/java/Tests.java
+++ Homework/java-core/13-Reflection-Api/src/test/java/Tests.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class Tests {
     @Test
@@ -58,7 +62,10 @@ class Tests {
     void get_properties_from_file_data() {
         byte[] fileData = "gh.inject.name=value".getBytes(StandardCharsets.UTF_8);
 
-        PropertyService propertyService = new PropertyService();
+        Logger logger = Logger.getLogger(PropertyService.class.getName());
+        logger.addHandler(new ConsoleHandler());
+        PropertyService propertyService = new PropertyService(logger);
+
         List<Property> expectedProperties = List.of(
             new Property("name", "value")
         );
@@ -72,9 +79,33 @@ class Tests {
     @Test
     @Tag("PropertyService")
     void Incorrect_to_get_properties_from_file_data_if_it_equal_to_null() {
-        PropertyService propertyService = new PropertyService();
+        Logger logger = Logger.getLogger(PropertyService.class.getName());
+        logger.addHandler(new ConsoleHandler());
+        PropertyService propertyService = new PropertyService(logger);
         assertThatThrownBy(() -> propertyService.getPropertiesFromFile(null))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("File data can't be null");
+    }
+
+    @Test
+    @Tag("PropertyService")
+    void get_properties_from_file_incorrect_line() {
+        byte[] fileData = "asd\nname=value".getBytes(StandardCharsets.UTF_8);
+
+        Logger logger = mock(Logger.class);
+
+        PropertyService propertyService = new PropertyService(logger);
+        List<Property> expectedProperties = List.of(
+            new Property("name", "value")
+        );
+
+        List<Property> properties = propertyService.getPropertiesFromFile(fileData);
+
+        assertThat(properties)
+            .isEqualTo(expectedProperties);
+
+        verify(logger).warning(
+            "asd:Incorrect input line format. It must contain exactly one '=' symbol\r\n"
+        );
     }
 }
