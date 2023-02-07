f117e3b079f5e5c8201a5d010c4b5d7b4c0d27e1
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-07 12:59:58 +0200

    HM11_UP:change retention time of @Injectable

--- Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/inject/Injectable.java
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/inject/Injectable.java
@@ -1,8 +1,11 @@
 package edu.geekhub.homework.inject;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Injectable {
     String propertyName() default "";
