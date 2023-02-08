2b5d51dd88e5dc193bda8c94cd43235623c3fb88
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-09 00:04:30 +0200

    HM11_UP:Update FieldUtil class

--- Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/reflection/FieldUtil.java
+++ Homework/java-core/13-Reflection-Api/src/main/java/edu/geekhub/homework/reflection/FieldUtil.java
@@ -1,18 +1,30 @@
 package edu.geekhub.homework.reflection;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 public class FieldUtil {
     private FieldUtil() {
     }
 
     public static void setFieldValue(Object target, String fieldName, Object fieldValue) {
+        if (Objects.isNull(target) || Objects.isNull(fieldName) || Objects.isNull(fieldValue)) {
+            throw new IllegalArgumentException(
+                String.format("Passed argument:%s with value equal null", target)
+            );
+        }
         try {
             Field field = target.getClass().getDeclaredField(fieldName);
             field.setAccessible(true);
             field.set(target, fieldValue);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException(
+                String.format("Failed to set field value for field:%s in class:%s",
+                    fieldName,
+                    target.getClass().getName()
+                ),
+                e
+            );
         }
     }
 }
