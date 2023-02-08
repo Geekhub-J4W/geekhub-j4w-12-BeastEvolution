cb699c14f5bb1095ce2f96376f47aa297816f8d6
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-08 20:31:29 +0200

    HM11_UP:Fix tests with logging and style

--- Homework/java-core/13-Reflection-Api/src/test/java/Tests.java
+++ Homework/java-core/13-Reflection-Api/src/test/java/Tests.java
@@ -1,19 +1,19 @@
 import edu.geekhub.homework.entity.Property;
 import edu.geekhub.homework.entity.PropertyService;
+import edu.geekhub.homework.files.ResourceUtil;
+import edu.geekhub.homework.inject.InjectProcessor;
 import edu.geekhub.homework.parsers.PropertyParser;
-import edu.geekhub.homework.reflection.ReflectionSetter;
+import edu.geekhub.homework.reflection.FieldUtil;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class Tests {
     @Test
@@ -61,49 +61,67 @@ class Tests {
     @Test
     @Tag("PropertyService")
     void get_properties_from_file_data() {
+        //Arrange
         byte[] fileData = "gh.inject.name=value".getBytes(StandardCharsets.UTF_8);
 
-        Logger logger = Logger.getLogger(PropertyService.class.getName());
-        logger.addHandler(new ConsoleHandler());
+        Logger logger = mock(Logger.class);
         PropertyService propertyService = new PropertyService(logger);
 
         List<Property> expectedProperties = List.of(
             new Property("name", "value")
         );
-
+        //Act
         List<Property> properties = propertyService.getPropertiesFromFile(fileData);
 
+        //Assert
         assertThat(properties)
             .isEqualTo(expectedProperties);
+
+        verifyNoInteractions(logger);
     }
 
     @Test
     @Tag("PropertyService")
     void Incorrect_to_get_properties_from_file_data_if_it_equal_to_null() {
-        Logger logger = Logger.getLogger(PropertyService.class.getName());
-        logger.addHandler(new ConsoleHandler());
+        //Arrange
+        Logger logger = mock(Logger.class);
         PropertyService propertyService = new PropertyService(logger);
+
+        //Act,Assert
         assertThatThrownBy(() -> propertyService.getPropertiesFromFile(null))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("File data can't be null");
+
+        verifyNoInteractions(logger);
     }
 
     @Test
     @Tag("PropertyService")
-    void get_properties_from_file_incorrect_line() {
-        byte[] fileData = "asd\nname=value".getBytes(StandardCharsets.UTF_8);
+    void get_properties_with_incorrect_format_form_file_data() {
+        //Arrange
+        byte[] fileData = "asd".getBytes(StandardCharsets.UTF_8);
 
         Logger logger = mock(Logger.class);
 
         PropertyService propertyService = new PropertyService(logger);
-        List<Property> expectedProperties = List.of(
-            new Property("name", "value")
-        );
+        List<Property> expectedProperties = List.of();
 
+        //Act
         List<Property> properties = propertyService.getPropertiesFromFile(fileData);
 
+        //Assert
         assertThat(properties)
             .isEqualTo(expectedProperties);
+    }
+
+    @Test
+    @Tag("PropertyService")
+    void create_log_when_file_text_have_incorrect_property_line() {
+        byte[] fileData = "asd".getBytes(StandardCharsets.UTF_8);
+        Logger logger = mock(Logger.class);
+        PropertyService propertyService = new PropertyService(logger);
+
+        propertyService.getPropertiesFromFile(fileData);
 
         verify(logger).warning(
             "asd:Incorrect input line format. It must contain exactly one '=' symbol\r\n"
@@ -111,16 +129,19 @@ class Tests {
     }
 
     @Test
+    @Tag("FieldUtil")
     void Set_value_to_class_declared_field() {
+        //Arrange
         ObjectWithField target = new ObjectWithField("value");
         String fieldName = "field";
         String fieldValue = "newValue";
 
-        ReflectionSetter reflectionSetter = new ReflectionSetter();
         ObjectWithField expectedResult = new ObjectWithField("newValue");
 
-        reflectionSetter.setFieldValue(target, fieldName, fieldValue);
+        //Act
+        FieldUtil.setFieldValue(target, fieldName, fieldValue);
 
+        //Assert
         assertThat(target)
             .isEqualTo(expectedResult);
     }
