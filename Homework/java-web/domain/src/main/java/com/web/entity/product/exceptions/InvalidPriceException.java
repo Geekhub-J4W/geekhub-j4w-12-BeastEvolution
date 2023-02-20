42207cde3b2d46e3237237a083dbd23286094694
Bohdan Lifirenko <qp123456ex@gmail.com>
2023-02-20 22:35:18 +0200

    WEB:delete unnecessary InvalidPriceException

--- Homework/java-web/domain/src/main/java/com/web/entity/product/exceptions/InvalidPriceException.java
+++
@@ -1,8 +0,0 @@
-package com.web.entity.product.exceptions;
-
-public class InvalidPriceException extends RuntimeException {
-
-    public InvalidPriceException(String message) {
-        super(message);
-    }
-}
